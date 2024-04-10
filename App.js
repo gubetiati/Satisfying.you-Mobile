56ae26e3970cb5974175c5c5f4934aecef62d81c
gubetiati <gubetiati@gmail.com>
2024-04-10 17:46:01 -0300

    estilo drawer

--- App.js
+++ App.js
@@ -3,20 +3,38 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator, DrawerContentScrollView, DrawerItemList, DrawerItem } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 import Home from './src/screens/Home';
 import AcoesPesquisa from './src/screens/AcoesPesquisa';
 
-const Drawer = createDrawerNavigator();
+const Drawer = createDrawerNavigator()
+const Stack = createStackNavigator()
 
 const App = () => {
   return (
+    
     <NavigationContainer>
+    
     <Drawer.Navigator>
       <Drawer.Screen 
         name="Home" 
         component={Home} 
-        options={{
-          headerShown: true,
+        options= {estilos.header}
+      />
+      <Drawer.Screen 
+      name="AcoesPesquisa" 
+      component={AcoesPesquisa}
+      options = {{headerShown: false}} />
+    </Drawer.Navigator>
+    
+  </NavigationContainer>
+
+  );
+};
+
+const estilos = StyleSheet.create({
+    header: {
+        headerShown: true,
           headerStyle: {
             backgroundColor: '#2B1D62',
           },
@@ -24,13 +42,7 @@ const App = () => {
             display: 'none',
           },
           headerTintColor:'white'
-          
-        }} 
-      />
-      <Drawer.Screen name="AcoesPesquisa" component={AcoesPesquisa} />
-    </Drawer.Navigator>
-  </NavigationContainer>
-  );
-};
+    }
+})
 
 export default App;
